210b57518d6a901402d5a8d94b43bb9ae1958919
dtenebruso <dtenebruso92@gmail.com>
2017-08-11 10:34:19 -0400

    Moving to github for OS upgrade<Ubuntu -> Arch>

---
+++ char_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+	char str_a[10];
+
+	str_a[0] = 'H';
+	str_a[1] = 'E';
+	str_a[2] = 'L';
+	str_a[3] = 'L';
+	str_a[4] = 'O';
+	str_a[5] = 0;
+	str_a[6] = 'W';
+
+
+
+	printf(str_a);
+
+	return 0;
+}
